a0d441f25ef6bd6858c9845f391ade94b7f0d32c
Evanderjh <evanderjh@gmail.com>
2021-01-25 20:38:16 -0300

    Refatoração do BD

---
+++ backend/bd/bd.sql
@@ -0,0 +1,39 @@
+drop database if exists quiz_login;
+
+create database magnolia DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+use magnolia;
+
+CREATE TABLE usuaria(
+    id_usuaria int not null primary key AUTO_INCREMENT
+    nome varchar(180),
+    nascimento date,
+    telefone varchar(21),
+    telefone_contato varchar(21),
+    regiao varchar(80),
+    email varchar(252) unique not null,
+    senha varchar(60)
+)engine=InnoDB default charset=utf8;
+
+create table quiz_resultado(
+    id_resultado int not null primary key AUTO_INCREMENT, 
+    letra_escolhida varchar(10),
+    id_usuaria int not null,
+    foreign key(id_usuaria) references usuarias(id_usuaria)
+)engine=InnoDB default charset=utf8; 
+
+CREATE TABLE perguntas(
+id_pergunta int primary key not null auto_increment,
+enunciado_txt varchar(512) not null,
+enunciado_img varchar(512),
+respostaA varchar(512),
+respostaB varchar(512), 
+respostaC varchar(512),
+respostaD varchar(512),
+respostaE varchar(512),
+imagemA varchar(512),
+imagemB varchar(512),
+imagemC varchar(512),
+imagemD varchar(512),
+imagemE varchar(512)
+);
--- backend/bd/login.sql
+++
@@ -1,9 +0,0 @@
-create database quiz_login;
-
-create table quiz_login.cadastro(
-    id int not null primary key AUTO_INCREMENT,
-    nome varchar(80) not null,
-    idade int not null,
-    email varchar(80) not null UNIQUE,
-    senha char(32) not null
-);
